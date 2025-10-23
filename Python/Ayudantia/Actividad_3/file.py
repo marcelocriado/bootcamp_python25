41568e2264dd887d440aa87f0a21395764996082
Marcelo <criado71@gmail.com>
2025-10-22 22:44:33 -0300

    Realizacion de ejercicios de clases y ayudantia

--- Python/Ayudantia/Actividad_3/file.py
+++ Python/Ayudantia/Actividad_3/file.py
@@ -10,14 +10,30 @@ def suma(num1, num2):
 def rest(num1, num2):
     res = num1 - num2
     print("======================")
-    print(f"El resultado de la suma es: {res}")
+    print(f"El resultado de la resta es: {res}")
     print("======================")
     return(res)
+
+def multiplicar(num1, num2):
+    mult = num1 * num2
+    print("======================")
+    print(f"El resultado de la multiplicacion es: {mult}")
+    print("======================")
+    return(mult)
+
+def divicion(num1, num2):
+    div = num1 / num2
+    print("======================")
+    print(f"El resultado de la division es: {div}")
+    print("======================")
+    return(div)
 
 def mostrar_menu():
     print("======================")
     print("1) Sumar")
     print("2) Restar")
+    print("3) Multiplicar")
+    print("4) Dividir")
     print("0) Salir")
     print("======================")
 
@@ -40,6 +56,16 @@ def main ():
             num2 = float(input("Ingrese el segundo numero a restar: "))
             time.sleep(3)
             rest(num1, num2)
+        elif opcion == 3:
+            num1 = float(input("Ingrese el primer numero a multiplicar: "))
+            num2 = float(input("Ingrese el segundo numero a multiplicar: "))
+            time.sleep(3)
+            multiplicar(num1, num2)
+        elif opcion == 4:
+            num1 = float(input("Ingrese el primer numero a dividir: "))
+            num2 = float(input("Ingrese el segundo numero a dividir: "))
+            time.sleep(3)
+            divicion(num1, num2)
         else:
             print("La opcion no existe")
 
--- Python/Ejercicios/Ejercicios3/file.py
+++ Python/Ejercicios/Ejercicios3/file.py
@@ -1,3 +1,7 @@
+import time
+#===========================================================================
+#Filtros de estudiantes
+#===========================================================================
 # estudiantes = [ 
 #     {"Nombre": "Juan", "Edad": 17, "Calificaciones": [85, 90, 88]},
 #     {"Nombre": "Maria", "Edad": 19, "Calificaciones": [92, 89, 30]},
@@ -5,26 +9,33 @@
 #     {"Nombre": "Ana", "Edad": 18, "Calificaciones": [90, 92, 87]},
 #     {"Nombre": "Luis", "Edad": 20, "Calificaciones": [88, 85, 92]},
 # ]
+# def primo(edad):
+#     if edad <= 1:
+#         return False
+#     for i in range(2, int(edad**0.5) + 1):
+#         if edad % i == 0:
+#             return False
+#     return True
 
 # def filtro1():
 #     for estudiante in estudiantes:
-#         notas = estudiante["Calificaciones"]
-#         prom = sum(notas)/len(notas)
-        
-#         if estudiante["Edad"] >= 18:
-#             if prom >= 85.0:
-#                 print (f"El estudiante {estudiante["Nombre"]}, tiene un promedio de {prom:.2f}")
-#         else:
-#             print("No se han encontrado estudiantes con esas caracteristicas")
+#         suma = 0
+#         for nota in estudiante["Calificaciones"]:
+#             suma += nota
+#             prom = suma/len(estudiante["Calificaciones"])
+#             if estudiante["Edad"] >= 18:
+#                 if prom >= 85.0:
+#                     print (f"El estudiante {estudiante["Nombre"]}, tiene un promedio de {prom:.2f}")
+#             else:
+#                 print("No se han encontrado estudiantes con esas caracteristicas")
 
 # def filtro2():
-#     suma = 0
 #     for estudiante in estudiantes:
-#         notas = estudiante["Calificaciones"]
-#         suma += notas
-#         prom = suma/len(notas)
-
-#         if estudiante["Edad"] % 2 == 0:
+#         suma = 0
+#         for nota in estudiante["Calificaciones"]:
+#             suma += nota
+#             prom = suma/len(estudiante["Calificaciones"])
+#         if not primo(estudiante["Edad"]):
 #             print (f"El estudiante {estudiante["Nombre"]}, con edad {estudiante["Edad"]}, tiene un promedio de {prom:.2f}")
 
 # while True:
@@ -49,13 +60,66 @@
 #         filtro2()
 
 #===========================================================================
-#
+#Listas sin 0
 #===========================================================================
-num = [[1,2,3], [0,4,5], [4,0,1], [6,5,4]]
-lista = []
-def filtro():
-    for numeros in num:
-        if numeros[0] == 0:
-            continue
-        print(numeros)
-filtro()
+# num = [[1,2,3], [0,4,5], [4,0,1], [6,5,4]]
+# lista = []
+# lista_final = []
+# letras = "ABCDEFGHIJKLMNOPQRS"
+
+# def filtro():
+#     #Eliminar la sublista con 0 al inicio
+#     for numeros in num:
+#         if numeros[0] == 0:
+#             continue
+#     #Eliminar los 0 de la sub lista
+#         while 0 in numeros:
+#             numeros.remove(0)
+#         lista.append(numeros)
+#         for i, lista_nueva in enumerate(lista):
+#             etiqueta = letras[i]
+#         lista_final.append(f"{etiqueta}: {lista_nueva}")
+#     print("================================")
+#     print("La lista final es:")
+#     print(lista_final)
+#     print("================================")
+# filtro()
+
+#===========================================================================
+#Operaciones
+#===========================================================================
+def suma(num1, num2):
+    suma = num1 + num2
+    return(suma)
+
+def resta(num1, num2):
+    resta = num1 - num2
+    return(resta)
+
+def mult(num1, num2):
+    mult = num1 * num2
+    return(mult)
+
+def div(num1, num2):
+    div = num1 / num2
+    return(div)
+
+tupla = []
+while True:
+    print("====================")
+    print("Bienvenido a calcular Z")
+    print("Ingrese los numeros a calcular")
+    print("====================")
+
+    num1 = float(input("Ingrese el primer numero a calcular: "))
+    num2 = float(input("Ingrese el segundo numero a calcular: "))
+    time.sleep(3)
+    print("El resultado final es el siguiente.")
+    print(f"Suma: {suma(num1,num2)}")
+    print(f"Resta: {resta(num1,num2)}")
+    print(f"Multiplicacion: {mult(num1,num2)}")
+    print(f"Division: {div(num1,num2)}")
+    time.sleep(2)
+    print("Los resultados han sido almacenados")
+
+    tupla.append(f"Suma: {suma(num1,num2)}, Resta: {resta(num1,num2)}, Multiplicacion: {mult(num1,num2)}, Division: {div(num1,num2)}")
