a84647c14acb7fc1ae5d4ecd43778d7ab487e5a0
Marcelo <criado71@gmail.com>
2025-10-23 23:22:34 -0300

    Realizacion del primer core

---
+++ Python/Cores/bucle_for_básico1.py
@@ -0,0 +1,49 @@
+#===================
+#   Basico
+#===================
+# for i in range(101):
+#     print(i)
+
+#===================
+#   Multiplos de 2
+#===================
+# for i in range(2,501):
+#     if i % 2 == 0:
+#         print(i)
+
+#=================================
+#   Contando Vanilla Ice
+#=================================
+# for i in range(1,101):
+#     if i % 10 == 0:
+#         print("baby")
+#     elif i % 5 == 0:
+#         print("ice ice")
+#     else:
+#         print(i)
+
+#=================================
+#   Wow. Número gigante a la vista
+#=================================
+# total = 0
+# for i in range(500001):
+#     if i % 2 == 0:
+#         total += i
+
+# print(total)
+
+#====================
+#   Regrésame al 3
+#====================
+# for i in range(2025, -1, -3):
+#     print(i)
+
+#=======================
+#   Contador dinámico
+#=======================
+# numInicial = 2
+# numFinal = 202
+# multiplo = 4
+
+# for i in range(numInicial, numFinal+1, multiplo):
+#     print(i)
